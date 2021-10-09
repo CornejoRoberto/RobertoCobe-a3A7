ea6449976510d80d81eae1c80aef442f7ef8d7d1
CobeñaCornejoRoberto <roberto_samp@outlook.com>
2021-10-08 22:13:03 -0500

    Agregar archivos de proyecto.

---
+++ RobertoCobeña3A7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RobertoCobeña3A7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int con1 = 0; int con2 = 101;
+            do
+            {
+                con1 = con1 + 1;
+                con2 = con2 - 1;
+                Console.WriteLine(+con1);
+                Console.Write("\t");
+                Console.Write("");
+                Console.WriteLine(+con2);
+            } while (con1 < 100);
+        }
+    }
+}
+
